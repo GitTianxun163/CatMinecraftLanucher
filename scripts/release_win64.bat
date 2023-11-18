928726f789664674228f27cc5d56645b499896d2
GitTianxun163 <tianxunwx@163.com>
2023-11-18 13:59:18 +0800

    添加了检查部分

--- scripts/release_win64.bat
+++ scripts/release_win64.bat
@@ -1,20 +1,25 @@
 @echo off
 
-echo mocing
+echo Checking
+if not exist src (echo Didn't find src dictory && pause && exit)
+if not exist build (echo Diring build && mkdir build)
+if not exist bin\release_win64 (echo Diring bin\release_win64 && mkdir bin\release_win64)
+
+echo Mocing
 moc src/Application.h -o build/moc_Application.cpp
 moc src/Widgets/TitleBar.h -o build/moc_TitleBar.cpp
 moc src/Widgets/DailogButton.h -o build/moc_DailogButton.cpp
 
 echo Objecting
-g++ -c src/main.cpp -o build/main.o -I %QT_HOME%\include -I include
-g++ -c src/Application.cpp -o build/Application.o -I %QT_HOME%\include -I include
-g++ -c src/Widgets/TitleBar.cpp -o build/TitleBar.o -I %QT_HOME%\include -I include
-g++ -c src/Widgets/DailogButton.cpp -o build/DailogButton.o -I %QT_HOME%\include -I include
+g++ -c src/main.cpp -o build/main.o -I %QT_HOME%/include -I include
+g++ -c src/Application.cpp -o build/Application.o -I %QT_HOME%/include -I include
+g++ -c src/Widgets/TitleBar.cpp -o build/TitleBar.o -I %QT_HOME%/include -I include
+g++ -c src/Widgets/DailogButton.cpp -o build/DailogButton.o -I %QT_HOME%/include -I include
 
 echo Resources Complating
 windres -i src/Resources/resources.rc -o build/rc.o -F pe-x86-64
 
 echo Linking
-g++ build/*.o build/moc_*.cpp %QT_HOME%/lib/*.a -I %QT_HOME%\include -o bin/release_win64/lanucher.exe -Wl,--subsystem,windows -m64
+g++ build/*.o build/moc_*.cpp %QT_HOME%/lib/*.a -I %QT_HOME%/include -o bin/release_win64/lanucher.exe -Wl,--subsystem,windows -m64
 
 echo Complate Suffuily!
